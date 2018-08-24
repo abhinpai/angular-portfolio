b76dec959136649e49e78b278c5d254401131681
Abhin Pai <abhin.pai@honeywell.com>
2018-08-24 14:56:27 +0530

    Firebase configuration added in the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBT28oIpie9-HB38rWo-UYjeKJYmmsihGs",
+    authDomain: "angular-portfolio-e2cd6.firebaseapp.com",
+    databaseURL: "https://angular-portfolio-e2cd6.firebaseio.com",
+    projectId: "angular-portfolio-e2cd6",
+    storageBucket: "angular-portfolio-e2cd6.appspot.com",
+    messagingSenderId: "476113773047"
+  }
 };
