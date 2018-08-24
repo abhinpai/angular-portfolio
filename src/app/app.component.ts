5b9bfc3935c218183196e73d275f0bb3399dfd60
Abhin Pai <abhin.pai@honeywell.com>
2018-08-24 14:20:38 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public logos: any[];
+  
+  constructor() {}
+
+  ngOnInit(){
+   this.logos= [
+      {
+        name:'Google Plus',
+        path: '../assets/images/google-plus.svg'
+      },
+      {
+        name:'Facebook',
+        path: '../assets/images/facebook.svg'
+      },
+      {
+        name:'Github',
+        path: '../assets/images/github.svg'
+      },
+      {
+        name:'Pintrest',
+        path: '../assets/images/pintrest.svg'
+      }
+    ];
+  }
+
 }
